d394299412488dba237318a238a6adfccf3544dd
Bambamoussa <39279217+Bambamoussa@users.noreply.github.com>
2021-01-10 01:03:43 +0100

    Update Updates.java

--- StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/Updates.java
+++ StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/Updates.java
@@ -18,6 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
