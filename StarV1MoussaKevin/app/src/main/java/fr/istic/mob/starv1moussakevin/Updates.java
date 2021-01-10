e6550776421901cc77dfe29b88085ad830e37095
AngeKevinYEO <71631203+AngeKevinYEO@users.noreply.github.com>
2021-01-10 01:09:24 +0100

    Update Updates.java

--- StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/Updates.java
+++ StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/Updates.java
@@ -16,7 +16,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
-
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
