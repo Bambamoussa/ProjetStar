7d62ac1e6be4d6fa0f12928afa36fef0590344c1
Bambamoussa <39279217+Bambamoussa@users.noreply.github.com>
2021-01-10 01:05:41 +0100

    Update ProgressManager.java

--- StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/ProgressManager.java
+++ StarV1MoussaKevin/app/src/main/java/fr/istic/mob/starv1moussakevin/ProgressManager.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
-
 import androidx.core.app.NotificationCompat;
 
 public class ProgressManager {
